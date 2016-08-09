f90caa3ee3510ea989e28b883f99229108107524
Sonny Tosco <sonny.tosco@gmail.com>
2016-08-09 10:00:17 -0700

    solution for stars

--- sonnytosco_python_stars.py
+++ sonnytosco_python_stars.py
@@ -8,14 +8,43 @@
 # *****
 # *******
 # *********************************
-def draw_stars (a,var):
-    b=[]
-    for i in a:
-        i=i*var
-        b.append(i)
-    return b
-a=[2,4,10,16]
-for i in draw_stars(a,'*'):
-    if i in a!=input
-        c=len(arr[i])
-    print i
+# def draw_stars (a,var):
+#     b=[]
+#     for i in a:
+#         i=i*var
+#         b.append(i)
+#     return b
+# a=[2,4,10,16]
+# for i in draw_stars(a,'*'):
+#     print i
+
+# Modify the function above. Allow a list containing integers and strings to be passed
+# to the  draw_stars() function. When a string is passed, instead of  displaying *,
+# display the first letter of the string according to the example below.
+# You may use the .lower() string method for this part.
+
+# For example:
+# x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
+# draw_stars(x) should print the following in the terminal:
+# ****
+# ttt
+# *
+# mmmmmmm
+# *****
+# *******
+# jjjjjjjjjjj
+
+y = [4,'Tom',1,'Michael',5,7,'Jimmy Smith']
+def draw_stars2(my_list):
+    for item in my_list:
+        output = ''
+        if type(item) is int:
+            for i in range(item):
+                output += '*'
+        elif type(item) is str:
+            first_letter = item[0].lower()
+            for i in range(len(item)):
+                output += first_letter
+        print output
+
+draw_stars2(y)
