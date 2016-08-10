2bc663f394a655048967958497de1e70433b6d1a
Sonny Tosco <sonny.tosco@gmail.com>
2016-08-09 17:03:48 -0700

    quick update with regex. not complete

--- sonnytosco_python_names.py
+++ sonnytosco_python_names.py
@@ -1,17 +1,37 @@
 # Given the following list:
-students = [
-     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-     {'first_name' : 'John', 'last_name' : 'Rosales'},
-     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-     {'first_name' : 'KB', 'last_name' : 'Tonel'}
-]
-
-# Create a program that outputs:
-# Michael Jordan
-# John Rosales
-# Mark Guillen
-# KB Tonel
-print students.items()
-# for key, data in students.items():
-#     for value in data:
-#         print value["first_name"], value["last_name"]
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+#
+# # Create a program that outputs:
+# # Michael Jordan
+# # John Rosales
+# # Mark Guillen
+# # KB Tonel
+# for x in students:
+#     print x['first_name'], x['last_name']
+#
+# Now given the following dictionary:
+# users = {
+#  'Students': [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#   ],
+#  'Instructors': [
+#      {'first_name' : 'Michael', 'last_name' : 'Choi'},
+#      {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+#   ]
+#  }
+# for x in users:
+#     counter=0
+#     print x
+#     for y in users[x]:
+#         counter+=1
+#         name=y['first_name'].upper()+" "+y['last_name'].upper()
+#         length=len(y['first_name'])+len(y['last_name'])
+#         print "{}-{}-{}".format(counter, name, length)
