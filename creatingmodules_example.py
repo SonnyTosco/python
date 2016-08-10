3d982fcffb1a2565cedf8f680b38f889edf31534
Sonny Tosco <sonny.tosco@gmail.com>
2016-08-10 15:44:51 -0700

    final version of deck of cards with fisher yates

---
+++ creatingmodules_example.py
@@ -0,0 +1,11 @@
+# Creating Modules
+# Since arithmetic is another python file saved in the same folder we can
+# call the function from that file and use it in this file using import.
+
+# import arithmetic
+# print arithmetic.add(5,8)
+# print arithmetic.subtract(10,5)
+# print arithmetic.multiply(12,6)
+
+Packages
+- a module is a single file (or files) that are imported under one import. A
--- deckofcards.py
+++ deckofcards.py
@@ -5,19 +5,15 @@ class Card(object):
         self.suit=suit
         self.number=number
         self.visib=False
-
     def flip(self):
         if self.visib==True:
             self.visib=False
         else:
             self.visib=True
         return self
-
     def displayCard(self):
         print self.suit, str(self.number)
         return ""
-
-
 class Deck(object):
     def __init__(self):
         self.cards = []
@@ -29,70 +25,71 @@ class Deck(object):
         for card_suit in suits:
             for card_value in types:
                 self.cards.append(Card(card_suit, card_value))
-                # print card_suit, card_value
-        return self
-    def shuffle(self):
-        random.shuffle(self.cards)
-        return self
-    def deal(self, player):
-        if len(self.cards)==0:
-            print "No more cards."
-            return self
-        player.hand.append(self.cards[-1])
-        print player.hand
-        self.cards.pop()
         return self
 
+    def shuffle(self):
+        if len(self.cards)>1:
+            i=len(self.cards)-1
+            while i > 0:
+                s = random.randint(0,i)
+                self.cards[i],self.cards[s] = self.cards[s], self.cards[i]
+                i-=1
+
+    def deal(self):
+        if len(self.cards)==0:
+            return false
+        else:
+            return self.cards.pop()
     def show_Deck(self):
         for i in self.cards:
             i.displayCard()
         return self
-
 class Player(object):
     def __init__(self,name):
         self.name = name
         self.hand = []
-
-    def draw(self,deck):
-        if len(deck.cards)==0:
-            print "No more cards."
-            return self
-        self.hand.append(deck.cards[-1])
-        deck.cards.pop()
+    def draw(self, deck):
+        card = deck.deal()
+        if card:
+            self.hand.append(card)
+        else:
+            print "no cards in deck"
         return self
-
     def discard(self):
         self.hand.pop()
-
     def show_hand(self):
         for i in self.hand:
             i.displayCard()
-
-
-
-
-
-
-
-
-
-
-
+##-------------------
+##CODE BELOW EXECUTES
+##-------------------
 
 Elliot_deck = Deck()
 
-Elliot_deck.shuffle() #Deck.shuffle()
+Elliot_deck.shuffle()
 
 print "Cards in Deck:", len(Elliot_deck.cards)
 ricky = Player("ricky")
-Elliot_deck.deal(ricky).deal(ricky).deal(ricky)
+
+##Ricky draws cards here 3 times
+ricky.draw(Elliot_deck).draw(Elliot_deck).draw(Elliot_deck)
+
 print "---------------------"
 print "Below is Ricky's Hand:"
 print "---------------------"
+
+
 ricky.show_hand()
-print len(ricky.hand)
+print len(ricky.hand), "Cards in Ricky's hand, shown above."
+
+
+
 print "---------------------"
 print "Cards in Deck:", len(Elliot_deck.cards)
-print "---------------------", "below is the new deck"
+print "---------------------"
+print "below is the new deck"
+
+
+
 Elliot_deck.build()
 print len(Elliot_deck.cards)
