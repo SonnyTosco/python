aed49c5357f8cead78f16c0f12a1979093bf287d
Sonny Tosco <sonny.tosco@gmail.com>
2016-08-10 18:02:16 -0700

    finished animal

--- sonnytosco_python_animal2.py
+++ sonnytosco_python_animal2.py
@@ -8,3 +8,17 @@ class Dog(Animal):
         return self
 dog1=Dog("Debo",150)
 dog1.walk().walk().walk().run().run().pet().displayHealth()
+
+class Dragon(Animal):
+    def __init__(self, name, health):
+        self.name=name
+        self.health=170
+    def fly(self):
+        self.health-=10
+        return self
+    # def displayHealth(self):
+    #     print "This is a dragon!"
+dragon1=Dragon("Dragonzord", 170)
+dragon1.walk().walk().walk().run().run().fly().fly()
+print "This is a dragon!"
+dragon1.displayHealth()
