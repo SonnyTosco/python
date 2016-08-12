140979b823b72959d414b07024fed8a6fb17c688
Sonny Tosco <sonny.tosco@gmail.com>
2016-08-12 14:12:42 -0700

    wrapping up counter

--- counter/counter.py
+++ counter/counter.py
@@ -10,18 +10,16 @@ def index():
     session['counter']=session['counter']+1
     return render_template('index.html')
     return redirect('/')
-@app.route('/ninja')
+@app.route('/ninja', methods=['POST'])
 def ninja():
     print "all good"
     if session.get('counter')==None:
         session['counter']=0
     session['counter']=session['counter']+1
     return redirect('/')
-@app.route('/zero')
-def ninja():
+@app.route('/zero', methods=['POST'])
+def zero():
     print "all good"
-    if session.get('counter')==None:
-        session['counter']=0
-    session['counter']=session['counter']+1
+    session['counter']=None
     return redirect('/')
 app.run(debug=True)
