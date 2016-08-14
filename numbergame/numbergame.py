2579c4b882716f1d0f4a43393e2f628ff1873e78
Sonny Tosco <sonny.tosco@gmail.com>
2016-08-13 18:28:40 -0700

    finished number game close as i could

--- numbergame/numbergame.py
+++ numbergame/numbergame.py
@@ -1,27 +1,22 @@
+import random
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'very secret'
 
 @app.route('/')
 def index():
-    print "g2g"
+    if session.get('number')==None:
+        session['number']=random.randrange(0,101)
+        session['guess'] = None
+    print session['number']
     return render_template('index.html')
 @app.route('/enter', methods=['POST'])
-def ninja():
-    print "all good"
+def enter():
+    print "enter"
+    session['guess'] = request.form['guess']
     return redirect('/')
-@app.route('/fail', methods=['POST'])
-def ninja():
-    print "fail good"
-    if session.get('counter')==None:
-        session['counter']=0
-    session['counter']=session['counter']+1
-    return redirect('/')
-@app.route('/win', methods=['POST'])
-def ninja():
-    print "win good"
-    if session.get('counter')==None:
-        session['counter']=0
-    session['counter']=session['counter']+1
+@app.route('/reset', methods=['GET'])
+def reset():
+    session.clear()
     return redirect('/')
 app.run(debug=True)
