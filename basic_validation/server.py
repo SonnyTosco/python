33e3b652f8cfc665d7283b1b7364c07b0a4f6a76
Sonny Tosco <sonny.tosco@gmail.com>
2016-08-15 09:51:31 -0700

    starting advanced form validation

--- basic_validation/server.py
+++ basic_validation/server.py
@@ -3,12 +3,14 @@ app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
 @app.route('/')
 def index():
-  return render_template('index.html')
+    print "g2g"
+    return render_template('index.html')
 @app.route('/process', methods=['POST'])
 def process():
-  if len(request.form['name']) < 1:
-    flash("Name cannot be empty!")
-  else:
-    flash("Success! Your name is {}".format(request.form['name']))
-  return redirect('/') # either way the application should return to the index and display the message
+    print "Gucci"
+    if len(request.form['name']) < 1:
+        flash("Name cannot be empty!")
+    else:
+        flash("Success! Your name is {}".format(request.form['name']))
+    return redirect('/') # either way the application should return to the index and display the message
 app.run(debug=True)
